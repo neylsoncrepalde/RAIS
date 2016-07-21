196d06f5117f8d502536a35b053514b57f55101a
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-07-21 01:45:45 -0300

    Update rais_read.R

--- rais_read.R
+++ rais_read.R
@@ -1,16 +1,19 @@
 # Rodando a RAIS
 # Neylson Crepalde
 
-#library(readr)
-
 setwd("~/Documentos/Neylson Crepalde/RAIS 2014")
 ###################################################
-
-#rais <- read_csv2("MG2014.txt", col_names = T, n_max = 10000)
-#rais[5894:5900,]
+rais.head <- read.csv2("MG2014.txt", nrows = 10, fileEncoding = "latin1")
+heading <- names(rais.head)
 #############################################
 
 library(ff)
-system.time(rais <- read.table.ffdf(file="MG2014.txt",sep=";",dec=",",na.strings = c('{ñclass}','{ñ','{ñ class}','{ñ c'), fileEncoding="latin1"))
+library(ffbase)
+system.time(rais <- read.table.ffdf(file="MG2014.txt",sep=";",dec=",",na.strings = c('{ñclass}','{ñ','{ñ class}','{ñ c'), 
+                                    fileEncoding="latin1", header=T))
+dim(rais)
+rais[1,]
+rais$target <- ffifelse(rais$V36 > 1000, 1L, 0L)
 
-
+library(biglm)
+biglinear <- biglm(V36~V19+V20, data=rais)
